7448bf9d697296de8b37b9eadca41b97913aee46
Jacob Haimes <jacob_haimes@taylor.edu>
2019-06-14 22:29:47 -0400

    Added jumbotron and buttons to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="home">
-
+    <div class="jumbotron">
+      <h1 class="display-4">ONWARD!</h1>
+      <p class="lead">Simple app to stay on track</p>
+      <hr class="my-4">
+      <p>TEMP</p>
+      <a class="btn btn-primary btn-lg" href="/" role="button">Login</a>
+      <a class="btn btn-primary btn-lg" href="/" role="button">Sign Up</a>
+    </div>
   </div>
 </template>
 
