5570df7d6de3517d380b9cedea3e144cf2515af2
Jacob Haimes <jacob_haimes@taylor.edu>
2019-06-15 03:09:19 -0400

    adding images and modified styles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="home">
     <div class="jumbotron">
-      <h1 class="display-4">ONWARD!</h1>
-      <p class="lead home-info">Simple app to stay on track</p>
-      <hr class="my-4 home-line">
-      <p class="home-info2">TEMP</p>
+      <h1 class="display-4 text-right">ONWARD!</h1>
+      <p class="lead home-info text-right">Simple app to stay on track</p>
+      <p class="home-info2 text-right">TEMP</p>
       <router-link class="btn btn-primary btn-lg home-btn" :to="{name: 'Auth'}" role="button" >Login/SignUp</router-link>
 
     </div>
