5445203a0feb238c2a28ef3f4982fdd4a93f43ed
Jacob Haimes <jacob_haimes@taylor.edu>
2019-06-14 22:02:01 -0400

    added mutations and actions demo

--- src/store/users.js
+++ src/store/users.js
@@ -1,10 +1,27 @@
 const state = {
+
 }
 
 const mutations = {
+  /*
+  increment (state, n) {
+  state.count += n
+}
+  store.commit('increment', 10)
+  */
+
 }
 
 const actions = {
+  /*
+  actions: {
+  increment ({ commit }) {
+    commit('increment')
+  }
+
+  store.dispatch('increment')
+  */
+}
 }
 
 export default {
