a9f168f33448fca0f7655edb15732ca61f37a36f
Daniel Peach <dpeaches96@gmail.com>
2019-06-15 10:10:05 -0400

    Added future features

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,18 @@
     <p class="home-info2 text-right">TEMP</p>
     <router-link class="btn btn-primary btn-lg home-btn" :to="{name: 'Auth'}" role="button">Login/SignUp</router-link>
   </div>
+    <div>
+      <h2>Future features</h2>
+      <h4>
+        <ul>
+          <li>Preset milestones that allow for starting progress</li>
+          <li>DOB inclusion: different older age groups have access to more/ different preset milestones</li>
+          <li>Recurring milestones based on date: Do taxes... ect</li>
+          <li>Data set integration from key words.  Add resources to milestones to help goal achievement</li>
+        </ul>
+      </h4>
+
+    </div>
 </div>
 </template>
 
