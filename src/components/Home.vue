599dc56e7a30c3def7336387312c60ebe5c1c100
Daniel Peach <dpeaches96@gmail.com>
2019-06-15 13:08:13 -0400

    added FF

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,8 @@
     <h1 class="display-3 home-title">ONWARD!</h1>
     <router-link class="btn btn-danger btn-lg home-btn" :to="{name: 'Auth'}" role="button">Login/SignUp</router-link>
   </div>
-    <div>
+    <div class="jumbotron text-left " style="margin-top: 0.000001%">
+    <!--<div>-->
       <h2 class="display-5 text-left">About this site</h2>
         <ul>
           <li>Using a reward based system dramatically increases a users desire to accomplish tasks </li>
@@ -21,6 +22,7 @@
           <li>DOB inclusion: different older age groups have access to more/ different preset milestones</li>
           <li>Recurring milestones based on date: Do taxes... ect</li>
           <li>Location based milestone verification that are worth more points</li>
+          <li>Integration between milestones and calender programs</li>
         </ul>
 
 
