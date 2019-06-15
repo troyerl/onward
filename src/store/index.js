c9160b70f93b436b2e3d4350af685a96a3e6a4c6
Daniel Peach <dpeaches96@gmail.com>
2019-06-14 21:48:37 -0400

    added template users.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,22 @@
 import fb from '../fb'
+
+import users from './users'
+import conversations from './conversations'
+
+import config from '../config'
+
+firebase.initializeApp(config)
+
+Vue.use(Vuex)
+
+const state = {
+  db: firebase.firestore()
+}
+
+export default new Vuex.Store({
+  state,
+  modules: {
+    users,
+    conversations
+  }
+})
