c57ff8ea93cf53584562c5f9a0a3c3be6bc9544d
Jacob Haimes <jacob_haimes@taylor.edu>
2019-06-15 01:02:44 -0400

    RMC

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,12 @@
 <template>
   <div id="home">
     <div class="jumbotron">
-<<<<<<< HEAD
       <h1 class="display-4 home-h1">ONWARD!</h1>
       <p class="lead home-info">Simple app to stay on track</p>
       <hr class="my-4 home-line">
       <p class="home-info2">TEMP</p>
       <router-link class="btn btn-primary btn-lg home-btn" :to="{name: 'Auth'}" role="button" >Login/SignUp</router-link>
-=======
-      <h1 class="display-4">ONWARD!</h1>
-      <p class="lead">Simple app to stay on track</p>
-      <hr class="my-4">
-      <p>TEMP</p>
-      <router-link class="btn btn-primary btn-lg" :to="{name: 'Auth'}" role="button" v-for="link in authLinks">{{link}}</router-link>
->>>>>>> 4663d38e808c472050df86fffcf73118fefe6b35
+
     </div>
 
   </div>
