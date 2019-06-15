2b85a23a721ba3813a8ef8834ae118f72945de32
Logan Troyer <troyerlogan@gmail.com>
2019-06-14 21:48:39 -0400

    Added home route

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import routes from 'routes'
 
+import Home from '../components/Home'
+
 Vue.use(Router)
 
 const router = new Router({
@@ -10,6 +12,11 @@ const router = new Router({
         {
             path: '*',
             redirect: routes.index
+        },
+        {
+            path: routes.index,
+            name: 'Home',
+            component: Home
         }
     ]
 })
