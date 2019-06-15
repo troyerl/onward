1832f81f2c3fe1a8ff566c010d88d519b507f0b9
Jacob Haimes <jacob_haimes@taylor.edu>
2019-06-15 00:43:26 -0400

    modified a button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="home">
     <div class="jumbotron">
-      <h1 class="display-4">ONWARD!</h1>
-      <p class="lead">Simple app to stay on track</p>
-      <hr class="my-4">
-      <p>TEMP</p>
-      <router-link class="btn btn-primary btn-lg" :to="{name: 'Auth'}" role="button">Login</router-link>
-      <router-link class="btn btn-primary btn-lg" :to="{name: 'Auth'}" role="button">Sign Up</router-link>
+      <h1 class="display-4 home-h1">ONWARD!</h1>
+      <p class="lead home-info">Simple app to stay on track</p>
+      <hr class="my-4 home-line">
+      <p class="home-info2">TEMP</p>
+      <router-link class="btn btn-primary btn-lg home-btn" :to="{name: 'Auth'}" role="button" >Login/SignUp</router-link>
     </div>
 
   </div>
