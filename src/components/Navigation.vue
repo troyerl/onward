9a1cbdca06878238b5a839a455c96c5ddc9195a4
Daniel Peach <dpeaches96@gmail.com>
2019-06-15 00:50:09 -0400

    added to Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="navigation">
+    <ul class="nav">
+      <li class="nav-item">
+        <router-link class="nav-link" :to="{name: 'Home'}">Onward!</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" href="#" :to="{name: 'Home'}">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" :to="{name: 'Auth'}">Login/Sign-Up</router-link>
+      </li>
+    </ul>
+
 
   </div>
 </template>
 
 <script>
-  export default {
+  import {mapstate} from 'vuex'
 
+  export default {
+    /*computed: {
+      ...mapstate(['currentUser'])
+    }*/
   }
 </script>
