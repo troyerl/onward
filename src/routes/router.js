88d1d5141ff69b5189ef10087d8a325df9f560a1
Daniel Peach <dpeaches96@gmail.com>
2019-06-15 07:07:37 -0400

    fixed routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -13,69 +13,72 @@ import MilestonesForm from "../components/MilestonesForm";
 Vue.use(Router)
 
 const router = new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '*',
-            redirect: '/'
-        },
-        {
-            path: '/',
-            name: 'Home',
-            component: Home
-        },
-        {
-            path: '/auth',
-            name: 'Auth',
-            component: Auth,
-            meta: {
-                requiresAuth: false
-            }
-        },
-        {
-            path: '/user/dashboard',
-            name: 'Dashboard',
-            component: Dashboard,
-            meta: {
-                requiresAuth: true
-            }
-        },
-        {
-            path: '/user/profile',
-            name: 'Profile',
-            component: Profile,
-            meta: {
-                requiresAuth: true
-            }
-        },
-        {
-            path: '/user/milestones',
-            name: 'Milestone',
-            component: Milestones
-        },
-        {
-            path: '/user/milestones-form',
-            name: 'MilestoneForm',
-            component: MilestonesForm,
-            meta:{
-                requiresAuth: true
-            }
-}
-]
+  mode: 'history',
+  routes: [
+    {
+      path: '*',
+      redirect: '/'
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/auth',
+      name: 'Auth',
+      component: Auth,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/user/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/user/profile',
+      name: 'Profile',
+      component: Profile,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/user/milestones',
+      name: 'Milestone',
+      component: Milestones,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/user/milestoneForm',
+      name: 'MilestoneForm',
+      component: MilestonesForm,
+      meta: {
+        requiresAuth: true
+      }
+    }
+  ]
 })
 
 router.beforeEach((to, from, next) => {
-    const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
-    const routeIsLogin = to.matched.some(x => x.path === '/auth')
-    const currentUser = firebase.auth().currentUser
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  const routeIsLogin = to.matched.some(x => x.path === '/auth')
+  const currentUser = firebase.auth().currentUser
 
-    if (requiresAuth && !currentUser) {
-        next('/login')
-    } else if (routeIsLogin && currentUser) {
-        next('dashboard')
-    } else {
-        next()
-    }
+  if (requiresAuth && !currentUser) {
+    next('/login')
+  } else if (routeIsLogin && currentUser) {
+    next('dashboard')
+  } else {
+    next()
+  }
 })
 
 export default router
