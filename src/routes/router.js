b1384d5626e18f40192b9f59120ac8e4d3f81494
Daniel Peach <dpeaches96@gmail.com>
2019-06-15 05:26:44 -0400

    added route gaurds

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import firebase from 'firebase'
 import routes from './routes'
 import Home from '../components/Home'
 import Auth from '../components/Auth'
 import Dashboard from "../components/Dashboard/Dashboard";
 import Profile from "../components/Dashboard/Profile";
+
+
 
 Vue.use(Router)
 
@@ -24,19 +26,42 @@ const router = new Router({
     {
       path: '/auth',
       name: 'Auth',
-      component: Auth
+      component: Auth,
+      meta: {
+        requiresAuth: false
+      }
     },
     {
       path: '/user/dashboard',
       name: 'Dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/user/dashboard/profile',
       name: 'Profile',
-      component: Profile
+      component: Profile,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  const routeIsLogin = to.matched.some(x => x.path === '/auth')
+  const currentUser = firebase.auth().currentUser
+
+  if (requiresAuth && !currentUser) {
+    next('/login')
+  } else if (routeIsLogin && currentUser){
+    next('dashboard')
+  } else {
+    next()
+  }
 })
 
 export default router
