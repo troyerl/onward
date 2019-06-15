b399dc0591c136a57e8aa2be2bb4d89025eb891a
Daniel Peach <dpeaches96@gmail.com>
2019-06-15 13:46:36 -0400

    Updated

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -13,13 +13,40 @@
     </div>
 
     <h3 class="mt-5">Current Milestone: </h3>
+    <div class="row">
+      <div class="col">
+        <div class="jumbotron jumbo-example text-left" v-for="ms in milestones">
+          <h3 class="mb-4">{{ms.title}}</h3>
+          <hr>
+          <div class="accordion" :id="'m' + ms.ref.id | first5">
+            <div class="card" v-for="task in ms.tasks" style="cursor: pointer">
+              <div class="card-header" id="headingOne" data-toggle="collapse"
+                   :data-target="'#t' + task.ref.id | first5">
+                <p class="mb-0 d-flex justify-content-between align-items-center font-weight-bold text-danger">
+                  {{task.title}}
+                  <span class="badge badge-dark badge-pill">points: {{task.points}}</span>
+                </p>
+              </div>
+              <Resources :task="task" :ms="ms"></Resources>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import { mapState, mapActions } from 'vuex'
+  import { mapState, mapActions, mapMutations } from 'vuex'
+
+  import Resources from '../components/Resources'
+  import fb from '../fb'
+
   export default {
     name: "Dashboard",
+    component: {
+      Resources
+    },
     data() {
       return {
       }
@@ -28,13 +55,45 @@
       ...mapState('user', {
         profile: state => state.userProfile,
         currentMS: state => state.currentMS,
+        currentUser: state => state.currentUser,
       }),
+      ...mapState('milestones', {
+        milestones: state => state.milestones,
+      })
     },
     methods: {
-
+      ...mapMutations('milestones', {
+        updateMilestones: UPDATE_MILESTONES,
+      }),
+      ...mapMutations('user', {
+        updateCurrentMilestone: UPDATE_CURRENT_MILESTONE,
+      }),
       ...mapActions('milestones', [
               'completeTask'
       ])
+    },
+    created() {
+      fb.db.collection('users').doc(this.currentUser.uid).onSnapshot((snapshot) => {
+        let msRefs = snapshot.data().milestones
+        let milestones = []
+        msRefs.forEach((msRef) => {
+          msRef.get().then((msDoc) => {
+            console.log(msDoc.data())
+            let taskRefs = msDoc.data().tasks
+            let tasks = []
+            taskRefs.forEach((taskRef) => {
+              taskRef.get().then((taskDoc) => {
+                let task = {...taskDoc.data(), ref: taskRef}
+                tasks.push(task)
+              })
+            })
+            let milestone = {...msDoc.data(), tasks: tasks, ref: msRef}
+            milestones.push(milestone)
+          })
+        })
+        this.updateMilestones(milestones)
+        this.updateCurrentMilestone(milestones[0])
+      })
     }
   }
 </script>
