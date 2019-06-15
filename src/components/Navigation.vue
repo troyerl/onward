0385b4c1b64b6ad3dd36000b7937e11321a1eb09
Daniel Peach <dpeaches96@gmail.com>
2019-06-15 03:02:29 -0400

    Added to nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -10,6 +10,16 @@
       <li class="nav-item">
         <router-link class="nav-link" :to="{name: 'Auth'}">Login/Sign-Up</router-link>
       </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Dropdown</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Link 1</a>
+          <a class="dropdown-item" href="#">Link 2</a>
+          <a class="dropdown-item" href="#">Link 3</a>
+        </div>
+      </li>
+
     </ul>
 
 
